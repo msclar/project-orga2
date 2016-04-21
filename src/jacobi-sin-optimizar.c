3d161c3b3bec982031a8188d19be15f124895625
Melanie Sclar <melaniesclar@gmail.com>
2016-04-20 21:48:17 -0300

    Jacobi debuggeado (lo demas no)

--- src/jacobi-sin-optimizar.c
+++ src/jacobi-sin-optimizar.c
@@ -10,16 +10,25 @@ int rho_b = 1060;
 double w_b = 0.00715;
 double T_a = 37.0;
 
-int max_i = 20, max_j = 20; // numeros al azar
+int max_i = 3, max_j = 3; // numeros al azar
 double delta_t = 0.1, delta_x = 0.1, delta_y = 0.1; // numeros al azar
 	
 int indice (int i, int j, int max_j) {
 	return i * max_j + j;
 }
 
-void jacobiStep(double Tn_sig[][max_j], 
-				double Tn[][max_j], 
-				double TInd[][max_j], 
+void print1D(double matrix[]) {
+	int i;
+	for (i = 0; i < max_i * max_j; i++) {
+		printf("%f\n", matrix[i]);
+	}
+	printf("\n");
+}
+
+void jacobiStep(double Tn_sig[], 
+				double Tn[],
+				double B[],
+				double TInd[], 
 				double A[][max_i * max_j]) {
 	// X_i^(iter+1) = (b_i - sum (j != i) a_ij * X_j^iter	
 	int i, j, i_a, j_a;
@@ -30,20 +39,21 @@ void jacobiStep(double Tn_sig[][max_j],
 			int s = indice(i, j, max_j);
 			for (i_a = 0; i_a < max_i; i_a++) {
 				for (j_a = 0; j_a < max_j; j_a++) {
-					sum += A[s][indice(i_a, j_a, max_j)] * Tn[i_a][j_a];
+					sum += A[s][indice(i_a, j_a, max_j)] * Tn[indice(i_a, j_a, max_j)];
 				}
 			}
-			sum -= A[s][s] * Tn[i][j];
-			
-			Tn_sig[i][j] = Tn[i][j] - TInd[i][j] - sum;
+			sum -= A[s][s] * Tn[s];
+			Tn_sig[s] = (B[s] - sum) / A[s][s];
 		}
 	}
 }
 
 int main( int argc, char** argv ) {
-	double Tn[max_i][max_j];
-	double Tn_sig[max_i][max_j];
-	double TInd[max_i][max_j];
+	freopen("test.out", "w", stdout);
+	double Tn[max_i * max_j];
+	double Tn_sig[max_i * max_j];
+	double TInd[max_i * max_j];
+	double B[max_i * max_j];
 	double k[max_i][max_j];
 	double sigma[max_i][max_j];
 	double phi[max_i][max_j];
@@ -52,8 +62,9 @@ int main( int argc, char** argv ) {
 	int i, j;
 	for (i = 0; i < max_i; i++) {
 		for (j = 0; j < max_j; j++) {
-			Tn[i][j] = T_a;
-			Tn_sig[i][j] = T_a;
+			int s = indice(i, j, max_j);
+			Tn[s] = T_a;
+			Tn_sig[s] = T_a;
 			k[i][j] = 0.565;
 			sigma[i][j] = 0.75;
 			phi[i][j] = 1500 * i * delta_x * pow(M_E, - (delta_x * i - 2) * (delta_x * i - 2) - (delta_y * j - 2) * (delta_y * j - 2)); // estan bien usados los delta?
@@ -61,7 +72,7 @@ int main( int argc, char** argv ) {
 			double deriv_phi_x = (phi[i+1][j] - phi[i-1][j]) / (2 * delta_x);
 			double deriv_phi_y = (phi[i][j+1] - phi[i][j-1]) / (2 * delta_y);
 			double gradient_phi_quad = deriv_phi_x * deriv_phi_x + deriv_phi_y * deriv_phi_y;
-			TInd[i][j] = w_b * c_b * rho_b * T_a + q_ddd + sigma[i][j] * gradient_phi_quad;
+			TInd[s] = w_b * c_b * rho_b * T_a + q_ddd + sigma[i][j] * gradient_phi_quad;
 		}
 	}
 	
@@ -86,19 +97,25 @@ int main( int argc, char** argv ) {
 			A[s][indice(i, j-1, max_j)] = - derivada_k_y / (2 * delta_y) + k[i][j+1] / (delta_y * delta_y);
 		}
 	}
-	
+		
 	// resolucion por Jacobi
-	// CAMBIAR ITER POR EL CALCULO DE LA CONVERGENCIA
-	// QUE IMPRIMIMOS COMO OUTPUT?
 	int iter, n;
-	for (n = 0; n < 10; n++) {
-		if (n % 2 == 0) {
-			for (iter = 0; iter < 10; iter++)
-				jacobiStep(Tn_sig, Tn, TInd, A);
-		} 
-		else {
-			for (iter = 0; iter < 10; iter++)
-				jacobiStep(Tn, Tn_sig, TInd, A);
+	for (n = 0; n < 1; n++) {
+		for (i = 0; i < max_i * max_j; i++) {
+			B[i] = Tn[i] - TInd[i];
+		}
+	
+		for (iter = 0; iter < 100; iter++) {
+			// siempre empieza estando la info bien en Tn, Tn_sig es auxiliar
+			// por eso, pongamos una cantidad de iteraciones *par*
+			if (iter % 2 == 0) {
+				jacobiStep(Tn_sig, Tn, B, TInd, A);
+				print1D(Tn_sig);
+			} 
+			else {
+				jacobiStep(Tn, Tn_sig, B, TInd, A);
+				print1D(Tn);
+			}
 		}
 	}
 	
