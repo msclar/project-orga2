0f1b7827fa0cf49390f69b05a083735c40f35970
Melanie Sclar <msclar@ws1.labo3>
2016-05-24 17:19:22 -0300

    remuevo comentarios innecesarios de graficar

--- src/graficar.m
+++ src/graficar.m
@@ -1,10 +1,9 @@
+%% Load all files from folder and generate a plot of the surface for
+%% each one
+
 function graficar()
-
-
-%% Load all files from folder
     fnames = dir('output/*.out');
     numfids = length(fnames);
-    %vals = cell(1,numfids);
     for K = 1:numfids
         fnames(K).name
         sprintf('output/%s',fnames(K).name)
@@ -13,15 +12,6 @@ function graficar()
         
         f = figure('visible', 'on');
         surf(X);
-        %savefig(f, sprintf('images/T%d.fig', K));
-        %close(f)
-
-        %imwrite(surf(X), sprintf('images/T%d.png', K)); % 'images/T' num2str(K) '.png');
-        %savefig(sprintf('images/T%d.fig', K)); % 'images/T' num2str(K) '.png');
+        
         saveas(gca, sprintf('images/T%d.fig', K));
-        %break;
     end
-
- 
-    %figure
-    %surf(X);
